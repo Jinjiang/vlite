9ffd338c735d126734172d02a52bcb430c68bbd6
Jinjiang <zhaojinjiang@me.com>
2024-02-26 21:15:51 +0800

    support css modules

--- shared/plugins/css.ts
+++ shared/plugins/css.ts
@@ -1,12 +1,29 @@
+import postcss from 'postcss';
+import modules from 'postcss-modules';
 import { File, Plugin } from '../types';
 
-// TODO: get css modules working
+const parseCssModules = async (file: File) => {
+  let classNames
+  const result = await postcss(
+    [modules({ getJSON: (_, json) => {
+      classNames = json
+    } })]
+  ).process(
+    file.content, { from: file.name }
+  )
+  return {
+    code: result.css,
+    classNames
+  }
+}
 
-const t = (content: string) => {
+const t = (content: string, classNames?: object) => {
   return `
 const style = document.createElement('style');
 style.innerHTML = ${JSON.stringify(content)};
 document.head.appendChild(style);
+
+export default ${JSON.stringify(classNames || {})};
 `.trim()
 }
 
@@ -16,10 +33,17 @@ const resolvedId = (id: string) => {
   }
 }
 
-const transform = (file: File) => {
+const transform = async (file: File) => {
+  let content = file.content
+  let classNames
+  if (file.name.endsWith('.module.css')) {
+    const result = await parseCssModules(file)
+    content = result.code
+    classNames = result.classNames
+  }
   return {
-    name: file.name.replace(/\.css?$/, '.js'),
-    content: t(file.content)
+    name: `${file.name}.js`,
+    content: t(content, classNames)
   }
 }
 
