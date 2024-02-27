70e72466e52dc7cf69aa0d57e81a17d466d459a9
Jinjiang <zhaojinjiang@me.com>
2024-02-27 11:51:09 +0800

    first css modules demo

--- vite-static/examples/App.tsx
+++ vite-static/examples/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import classNames from './style.module.css'
 
 export default function App() {
-  return <h1>Hello, World!</h1>;
+  return <h1 className={classNames.app}>Hello, World!</h1>;
 }
--- vite-static/examples/main.tsx
+++ vite-static/examples/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.tsx'
-import './style.css'
 
 console.log('Hello World!')
 
