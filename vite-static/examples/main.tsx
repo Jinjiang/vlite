268fe042229d9e3d237c5ff3d2c59fdb9dce9a5b
Jinjiang <zhaojinjiang@me.com>
2024-02-27 11:42:06 +0800

    first react demo

---
+++ vite-static/examples/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App.tsx'
+import './style.css'
+
+console.log('Hello World!')
+
+ReactDOM.render(<App />, document.getElementById('app'))
+
+// const h1 = document.createElement('h1')
+// h1.textContent = 'Hello World!'
+// document.body.appendChild(h1)
