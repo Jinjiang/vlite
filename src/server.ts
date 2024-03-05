3fc0167e98c529c7c6de42f4dc865d36fbe453eb
Jinjiang <zhaojinjiang@me.com>
2024-03-05 18:52:36 +0800

    refactor: server

--- src/server.ts
+++ src/server.ts
@@ -3,42 +3,57 @@ import path from 'path'
 import express, { Request, Response } from 'express'
 import mime from 'mime'
 import { compileRequest } from './compile.js'
+import { Context, Request as FileRequest } from './types.js'
 import { getExtName, codeExtNamesRegExp, getQuery } from './utils.js'
+
+const prepareRequest = (req: Request): FileRequest => {
+  const id = req.url
+  const pathname = req.url?.split('?')[0]
+  const name = pathname === '/' ? '/index.html' : pathname
+  const query = getQuery(req.url)
+  return {
+    id,
+    name,
+    query,
+  }
+}
+
+const prepareContext = (targetDir: string): Context => {
+  const defaultLoader = (req: FileRequest): string | Buffer => {
+    const { name } = req
+    const filename = path.resolve(targetDir, name.slice(1))
+    if (getExtName(name).match(codeExtNamesRegExp)) {
+      return fs.existsSync(filename) ? fs.readFileSync(filename, 'utf-8') : ''
+    }
+    return fs.existsSync(filename) ? fs.readFileSync(filename) : Buffer.from('')
+  }
+  return { defaultLoader, debug: false }
+}
 
 export const createServer = (targetDir: string, port: number) => {
   const resolvedTargetDir = targetDir || path.resolve('./')
   const resolvedPort = port || 3000
 
-  const defaultResolver = (id: string): string => {
-    return id === '/' ? '/index.html' : id
-  }
-
-  const defaultLoader = (id: string): string | Buffer => {
-    const filePathWithQuery = path.resolve(resolvedTargetDir, id.slice(1))
-    const filePath = filePathWithQuery.indexOf('?') > -1 ? filePathWithQuery.slice(0, filePathWithQuery.indexOf('?')) : filePathWithQuery
-    if (getExtName(id).match(codeExtNamesRegExp)) {
-      return fs.existsSync(filePath) ? fs.readFileSync(filePath, 'utf-8') : ''
-    }
-    return fs.existsSync(filePath) ? fs.readFileSync(filePath) : Buffer.from('')
-  }
+  const context = prepareContext(resolvedTargetDir)
 
   const requestHandler = async (req: Request, res: Response): Promise<void> => {
+    const fileRequest = prepareRequest(req)
 
     // Compile file
     const compiledFile = await compileRequest(
-      { id: req.url, name: req.url, query: {} },
-      { defaultLoader, debug: false }
+      fileRequest,
+      context
     )
 
     // Set Content-Type
-    if (req.url === '/' || req.url.endsWith('.html')) {
+    if (fileRequest.name.endsWith('.html')) {
       res.setHeader('Content-Type', 'text/html')
-    } else if (getExtName(req.url).match(codeExtNamesRegExp)) {
+    } else if (getExtName(fileRequest.name).match(codeExtNamesRegExp)) {
       res.setHeader('Content-Type', 'application/javascript')
-    } else if (getQuery(req.url).url) {
+    } else if (fileRequest.query.url) {
       res.setHeader('Content-Type', 'application/javascript')
     } else {
-      res.setHeader('Content-Type', mime.getType(req.url) || 'text/plain')
+      res.setHeader('Content-Type', mime.getType(fileRequest.name) || 'text/plain')
     }
 
     res.send(compiledFile)
